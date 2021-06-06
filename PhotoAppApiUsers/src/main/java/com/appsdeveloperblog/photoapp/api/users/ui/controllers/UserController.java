9c5e78a5435f35e83d58f5acc7465bf72432d5f7
Illya Sokolov <illya.sokolov82@gmail.com>
2021-06-06 18:38:57 +0300

    adding h2 database, start implementing Post User Request

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UserController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UserController.java
@@ -1,16 +1,27 @@
 package com.appsdeveloperblog.photoapp.api.users.ui.controllers;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.appsdeveloperblog.photoapp.api.users.ui.model.CreateUserRequestModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    Environment env;
+
     @GetMapping("/status/check")
     public String status() {
-        return "Working";
+        return "Working on port " + env.getProperty("local.server.port") + ", with token = " + env.getProperty("token.secret");
+    }
+
+    @PostMapping
+    public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+        return "Create user method is called";
     }
 }
